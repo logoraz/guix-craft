1965c8fd108274a56f7586ce660a10bed80d49c1
Erik Almaraz <Erik.Almaraz@svlabs.com>
2024-03-13 20:56:20 -0700

    guix:stumpwm bootstrap pt.3

---
+++ start-stumpwm.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# Disable access control for the current user
+xhost +SI:localuser:$USER
+
+# Disable touchpad/trackpad
+# https://packages.guix.gnu.org/packages/xinput/1.6.3/
+# https://askubuntu.com/questions/919495/how-to-disable-touchpad-completely-on-boot#answer-919496
+# xinput --set-prop 14 "Device Enabled" 0
+# xinput --set-prop 14 "Device Enabled" 1
+
+# Set fallback pointer
+xsetroot -cursor_name left_ptr
+
+# Fix scrolling on some GTK3 applications
+export GDK_CORE_DEVICE_EVENTS=1
+
+# Enable screen compositing
+picom &
+
+# Turn off the system bell
+# Doesn't seem to bee working...
+xset -b
+
+# Enable screen locking on suspend
+xss-lock -- slock &
+
+xrandr -l home
+
+# See: https://mail.gnu.org/archive/html/bug-guix/2023-04/msg00227.html
+# exec stumpwm
+exec sbcl --no-userinit --non-interactive --eval '(require :asdf)' --eval '(asdf:load-system "stumpwm")' --eval '(stumpwm:stumpwm)'
