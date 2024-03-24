3191568f91d3fc8d12ed22fa44ae9887d89d9baa
logoraz <erikalmaraz@fastmail.com>
2024-03-23 19:34:32 -0700

    Added Xorg configuration files in Xorg (WIP)...

---
+++ config/xorg/start-xterm.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# Setup:
+# ln -f ~/repos/guix-craft/config/xorg/xterm.sh \
+#       ~/.config/xorg/xterm.sh
+
+# Launch xterm in the background
+/run/current-system/profile/bin/xterm &
+
+# Sleep long enough to get the window open, and set transparency
+sleep .4s
+transset -a
