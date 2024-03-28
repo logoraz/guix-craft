a89aa214e3c5bfa9264d916181391076fa1b55b0
logoraz <erikalmaraz@fastmail.com>
2024-03-28 06:18:02 -0700

    Fixed Xorg lag, added prelim pass management to Nyxt (wip).

--- system.scm
+++ system.scm
@@ -36,8 +36,6 @@
  (kernel linux)
  ;; Fixes Xorg Lag - https://gitlab.com/nonguix/nonguix/-/issues/212
  (kernel-arguments (cons "i915.enable_psr=0" %default-kernel-arguments))
- (initrd microcode-initrd)
- (initrd-modules (cons "i915" %base-initrd-modules))
  (firmware (list linux-firmware))
  (locale "en_US.utf8")
  (timezone "America/Los_Angeles")
