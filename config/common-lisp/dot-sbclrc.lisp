462efa26fc45e2ce7217ad4815cda0adc6edd970
logoraz <erikalmaraz@fastmail.com>
2024-06-05 07:05:53 -0700

    Minor updates/experiments.

--- config/common-lisp/dot-sbclrc.lisp
+++ config/common-lisp/dot-sbclrc.lisp
@@ -1,11 +1,11 @@
 ;;;; dot-sbclrc.lisp -> .sbclrc - SBCL Initialization File
 
 ;;; The following lines added by ql:add-to-init-file:
-;; #-quicklisp
-;; (let ((quicklisp-init (merge-pathnames "quicklisp/setup.lisp"
-;;                                        (user-homedir-pathname))))
-;;   (when (probe-file quicklisp-init)
-;;     (load quicklisp-init)))
+#-quicklisp
+(let ((quicklisp-init (merge-pathnames "quicklisp/setup.lisp"
+                                       (user-homedir-pathname))))
+  (when (probe-file quicklisp-init)
+    (load quicklisp-init)))
 
 ;; Ref https://www.reddit.com/r/GUIX/comments/10ju937/quicklisp_on_guix/
 ;; Ref https://lists.gnu.org/archive/html/bug-guix/2020-01/msg00133.html
@@ -22,11 +22,11 @@
 ;;; The Guix Way...
 ;; Ref: https://github.com/atlas-engineer/nyxt/blob/master/documents/
 ;;      README.org#developers-installation-guide
-(require "asdf")
+;; (require "asdf")
 
-(let ((guix-profile (format nil "~a/.guix-profile/lib/" (uiop:getenv "HOME"))))
-  (when (and (probe-file guix-profile)
-             (ignore-errors (asdf:load-system "cffi")))
-    (push guix-profile
-          (symbol-value (find-symbol (string '*foreign-library-directories*)
-                                     (find-package 'cffi))))))
+;; (let ((guix-profile (format nil "~a/.guix-profile/lib/" (uiop:getenv "HOME"))))
+;;   (when (and (probe-file guix-profile)
+;;              (ignore-errors (asdf:load-system "cffi")))
+;;     (push guix-profile
+;;           (symbol-value (find-symbol (string '*foreign-library-directories*)
+;;                                      (find-package 'cffi))))))
