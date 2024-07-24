2a1890d0fbb6b177efe90d671022935c9b377e82
logoraz <erikalmaraz@fastmail.com>
2024-07-23 20:00:49 -0700

    Update deploy shell script.

--- config/stumpwm/deploy-stumpwm.sh
+++ config/stumpwm/deploy-stumpwm.sh
@@ -4,22 +4,22 @@
 # TODO - Once I learn guile well enough, will use Guix as a deploy agent for this...
 
 # Check if directory or directory link dne then create.
-stumpwm=~/.config/stumpwm
+stumpwm=~/.config/stumpwm/
 if [ ! -d $stumpwm ]; then
     echo "~/.config/stumpwm/ does not exist - creating..."
-    mkdir ~/.config/stumpwm/
+    mkdir $stumpwm
 fi
 
-stumpwm_modules=~/.config/stumpwm/data
+stumpwm_modules=~/.config/stumpwm/modules/
 if [ ! -d $stumpwm_modules ]; then
     echo "~/.config/stumpwm/modules/ does not exist - creating..."
-    mkdir ~/.config/stumpwm/modules/
+    mkdir $stumpwm_modules
 fi
 
-stumpwm_data=~/.config/stumpwm/data
+stumpwm_data=~/.config/stumpwm/data/
 if [ ! -d $stumpwm_data ]; then
     echo "~/.config/stumpwm/data/ does not exist - creating..."
-    mkdir ~/.config/stumpwm/data/
+    mkdir $stumpwm_data
 fi
 
 
@@ -28,15 +28,27 @@ ln -f ~/repos/guix-craft/config/stumpwm/config.lisp \
    ~/.config/stumpwm/config
 
 
-# Modules
+# Modules (StumpWM settings/configuration files)
 
 ## auto-start (Xorg)
 ln -f ~/repos/guix-craft/config/stumpwm/modules/auto-start.lisp \
    ~/.config/stumpwm/modules/auto-start.lisp
 
-## audio
-ln -f ~/repos/guix-craft/config/stumpwm/modules/audio-wpctl.lisp \
-   ~/.config/stumpwm/modules/audio-wpctl.lisp
+## commands
+ln -f ~/repos/guix-craft/config/stumpwm/modules/commands.lisp \
+   ~/.config/stumpwm/modules/commands.lisp
+
+## utilities
+ln -f ~/repos/guix-craft/config/stumpwm/modules/utilities.lisp \
+   ~/.config/stumpwm/modules/utilities.lisp
+
+## frames/windows
+ln -f ~/repos/guix-craft/config/stumpwm/modules/frames.lisp \
+   ~/.config/stumpwm/modules/frames.lisp
+
+## keybindings
+ln -f ~/repos/guix-craft/config/stumpwm/modules/keybindings.lisp \
+   ~/.config/stumpwm/modules/keybindings.lisp
 
 ## colors
 ln -f ~/repos/guix-craft/config/stumpwm/modules/colors.lisp \
@@ -50,30 +62,22 @@ ln -f ~/repos/guix-craft/config/stumpwm/modules/theme.lisp \
 ln -f ~/repos/guix-craft/config/stumpwm/modules/modeline.lisp \
    ~/.config/stumpwm/modules/modeline.lisp
 
-## frames/windows
-ln -f ~/repos/guix-craft/config/stumpwm/modules/frames.lisp \
-   ~/.config/stumpwm/modules/frames.lisp
 
-## keybindings
-ln -f ~/repos/guix-craft/config/stumpwm/modules/keybindings.lisp \
-   ~/.config/stumpwm/modules/keybindings.lisp
-
-## commands
-ln -f ~/repos/guix-craft/config/stumpwm/modules/commands.lisp \
-   ~/.config/stumpwm/modules/commands.lisp
-
-## utilities
-ln -f ~/repos/guix-craft/config/stumpwm/modules/utilities.lisp \
-   ~/.config/stumpwm/modules/utilities.lisp
-
-
-# StumpWM Contrib
-stumpwm_contrib=~/.local/share/stumpwm/contrib/
+# Stumpwm-contrib Packages
+stumpwm_contrib=~/.local/share/common-lisp/stumpwm-contrib/
 if [ ! -d $stumpwm_contrib ]; then
     echo "~/.local/share/stumpwm/contrib/ does not exist - creating..."
-    mkdir ~/.local/share/stumpwm/contrib/
+    mkdir $stumpwm_contrib
 fi
+
+## util & modeline: wpctl - Wireplumber Audio Controls for StumpWM
+ln -s ~/repos/guix-craft/config/stumpwm/libraries/wpctl \
+   ~/.local/share/common-lisp/stumpwm-contrib/wpctl
+
+## util: bluetooth
+ln -s ~/repos/guix-craft/config/stumpwm/libraries/bluetooth/ \
+   ~/.local/share/common-lisp/stumpwm-contrib/bluetooth
 
 ## util: end-session
 ln -s ~/repos/guix-craft/config/stumpwm/contrib/end-session \
-   ~/.local/share/stumpwm/contrib/end-session
+   ~/.local/share/common-lisp/stumpwm-contrib/end-session
