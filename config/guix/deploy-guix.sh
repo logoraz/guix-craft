06334817cb6334020bb2e94ebf335198cc147c3b
logoraz <erikalmaraz@fastmail.com>
2024-07-27 16:57:52 -0700

    Update guix deploy shell script.

--- config/guix/deploy-guix.sh
+++ config/guix/deploy-guix.sh
@@ -4,10 +4,10 @@
 # TODO - Once I learn guile well enough, will use Guix as a deploy agent for this...
 
 # Check if directory exists else create.
-guix=~/.config/guix/
-if [ ! -d $guix ]; then
-    echo "~/guix/ does not exist - creating..."
-    mkdir ~/guix/
+guix_config=~/.config/guix/
+if [ ! -d $guix_config ]; then
+    echo "~/.config/guix/ does not exist - creating..."
+    mkdir $guix_config
 fi
 
 # System
@@ -28,3 +28,11 @@ ln -f ~/repos/guix-craft/config/guix/dot-bashrc.sh \
 
 ln -f ~/repos/guix-craft/config/guix/dot-bash_profile.sh \
    ~/.config/guix/dot-bash_profile.sh
+
+# Manifests
+guix_manifests=~/.config/guix/manifests
+if [ ! -d $guix_manifests ] && [ ! -L $guix_manifests ]; then
+    echo "~/.config/guix/manifests does not exist - creating..."
+    ln -s ~/repos/guix-craft/config/guix/manifests \
+       $guix_manifests
+fi
