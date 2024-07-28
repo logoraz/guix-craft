9a11788ede059bf7a289be85522e2febd527cb04
logoraz <erikalmaraz@fastmail.com>
2024-07-27 22:46:28 -0700

    Clean up stumpwm utilities file.

--- config/stumpwm/modules/utilities.lisp
+++ config/stumpwm/modules/utilities.lisp
@@ -23,8 +23,3 @@
 
 ;;; Screenshots via Common Lisp -> removed scrot!
 (load-module "screenshot")
-
-;;; Notifications
-;; This module does not work -> crashes StumpWM when toggling notify server...
-;;(load-module "notify")
-;;(notify:notify-server-toggle)
