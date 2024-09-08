a81c7003e4dd92326cca8c8ecc8816e4e11c9990
logoraz <erikalmaraz@fastmail.com>
2024-09-07 19:34:59 -0700

    Update nyxt X transparency script.

--- config/nyxt/start-nyxt.sh
+++ config/nyxt/start-nyxt.sh
@@ -9,5 +9,5 @@ export GTK_THEME=Adwaita:dark
 /home/logoraz/.guix-home/profile/bin/nyxt &
 
 # Sleep long enough to get the window open, and set transparency
-sleep 1.40s
+sleep 1.75s
 transset -a 0.90
