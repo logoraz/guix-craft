cef39fd3bc4b893af8a4a7cfe0e73b0d025ffe50
logoraz <erikalmaraz@fastmail.com>
2024-10-13 08:54:00 -0700

    Update Lem config transparency (opacity).

--- config/lem/source/appearance.lisp
+++ config/lem/source/appearance.lisp
@@ -5,8 +5,7 @@
   (:export :set-opacity))
 (in-package :config/appearance)
 
-;; FIXME: Faling with the following error:
-;; 
+
 ;;; SDL2 specific
 #+lem-sdl2
 (progn
@@ -16,11 +15,13 @@
   (defun set-opacity (opacity)
     "Set SDL2 opacity, aka transparency."
     (sdl2-ffi.functions:sdl-set-window-opacity
-     (lem-sdl2/display:display-window (lem-sdl2/display:current-display)) (float opacity)))
+     (lem-sdl2/display:display-window (lem-sdl2/display:current-display)) 
+     (coerce opacity 'single-float)))
 
   (define-command toggle-opacity () ()
+    ;; FIXME: Weird bug (temp fix -- coerce)
     ;; The value 0.8 is not of type SINGLE-FLOAT when binding SB-ALIEN::VALUE
-    (set-opacity (if *opaque* 1 4/5))
+    (set-opacity (if *opaque* 1 0.8))
     (setf *opaque* (not *opaque*)))
   ;; Always start off as transparent
-  (set-opacity 4/5))
+  (set-opacity 0.8))
