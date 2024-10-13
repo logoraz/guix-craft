92f3981ef5fb5fe673cff977ba84ffa305b1475d
logoraz <erikalmaraz@fastmail.com>
2024-10-12 19:00:12 -0700

    Lem config update.

--- config/lem/source/misc.lisp
+++ config/lem/source/misc.lisp
@@ -4,27 +4,15 @@
   (:use :cl :lem))
 (in-package :config/misc)
 
-;;; TODO
 ;; Load Theme
 ;; (load-theme "decaf") ; default
 
 ;; Logs on the terminal output:
-;; (log:config :info)
+(log:config :info)
 
 ;;; Commands
 (define-command open-init-file () ()
   ;; @sasanidas
   (lem:find-file
    (merge-pathnames "init.lisp" (lem-home))))
-
-;;; Exploratory
-;; Version Control
-(define-command start-legit () ()
-  "Lem Command to start legit in thread."
-  (bt:make-thread
-   (lambda ()
-     (require :lem/legit)
-     ;; for some reason loading legit unsets keybindings and theme...
-     (load-theme "decaf")
-     (custom-keybindings))))
 
