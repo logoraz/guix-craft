e19b4b9b4d00f662f2092ef0941531160db62855
logoraz <erikalmaraz@fastmail.com>
2024-10-15 09:57:04 -0700

    Add notes for Lem learnings.

---
+++ config/lem/notes.lisp
@@ -0,0 +1,21 @@
+;;;; notes.lisp --> lem
+
+;;; Problem Statement:
+;;  |--> When connecting to a running micros instance (outside of Lem) on new :port
+;;       repl is successfully generated, connected to designated :port, however,
+;;       repl states:
+;;       |--> TBD
+
+;; lem/extensions/lisp-mode/lisp-mode.lisp
+
+(define-command slime-connect (hostname port &optional (start-repl t))
+    ((:splice
+      (list (prompt-for-string "Hostname: " :initial-value *localhost*)
+            (parse-integer
+             (prompt-for-string "Port: "
+                                :initial-value (princ-to-string *default-port*))))))
+  (let ((connection (connect-to-micros hostname port)))
+    (when start-repl (start-lisp-repl))
+    (connected-slime-message connection)))
+
+
